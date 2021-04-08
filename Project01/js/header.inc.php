e793e58436d7695cc01a3b2c55954145133ebdf1
andersonriley <rander9@ilstu.edu>
2021-04-08 16:59:23 -0500

    made some things
    yeah we did

--- Project01/js/header.inc.php
+++ Project01/js/header.inc.php
@@ -2,8 +2,8 @@
   <ul>
     <li><img class="logo" src="images/Logo3.png" alt="logo"></li>
     <li><a class="active" href="#home">Home</a></li>
-    <li><a href="Search.html">Search</a></li>
-    <li><a href="#contact">Write Review</a></li>
+    <li><a href="Search.php">Search</a></li>
+    <li><a href="ReviewPage.php">Write Review</a></li>
     <li class="floatRightPls"><a href="HomePage.html">Log Out</a></li>
   </ul>
 </header>

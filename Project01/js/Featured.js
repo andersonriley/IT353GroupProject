5752e6fcb3312c92ff309135ff69df9bcd23599d
andersonriley <rander9@ilstu.edu>
2021-04-08 16:37:33 -0500

    Made Some stuff
    Big changes

---
+++ Project01/js/Featured.js
@@ -0,0 +1,2 @@
+function display() {
+  var url = "http://www.omdbapi.com/?apikey=65bf2e0c&";
---
+++ Project01/js/TopNav.php
@@ -0,0 +1,9 @@
+<header>
+  <ul>
+    <li><img class="logo" src="images/Logo3.png" alt="logo"></li>
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="Search.html">Search</a></li>
+    <li><a href="#contact">Write Review</a></li>
+    <li class="floatRightPls"><a href="HomePage.html">Log Out</a></li>
+  </ul>
+</header>

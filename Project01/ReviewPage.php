11f1401059a746914a5137be88fa965e4bd1deca
andersonriley <rander9@ilstu.edu>
2021-04-08 17:11:27 -0500

    a
    aa

---
+++ Project01/ReviewPage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/Search.css">
+  </head>
+  <body>
+    <?php include 'header.inc.php' ?>
+  </body>
+</html>

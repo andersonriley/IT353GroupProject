78205ba9b9a5e637b3f892bb2a91c268f120fbc1
andersonriley <59894324+andersonriley@users.noreply.github.com>
2021-04-20 10:22:54 -0500

    changes to try to get Review page append to stay (unsuccessful)

--- Project01/ReviewPage.php
+++ Project01/ReviewPage.php
@@ -13,35 +13,65 @@
   <?php include 'js/header.inc.php' ?>
   <main>
     <h1>Sample Reviews</h1>
+
+
+<!-- START TRY 2 -->
+
+
+    <form onsubmit="addSampleReview()" class="sampleReview" method="post">
+      <select id='sampleCategory'>
+        <option>Movie</option>
+        <option>Book</option>
+        <option>Picture</option>
+        <option>Museum</option>
+      </select>
+      <br><input id='sampleTitle' type="text" name="" value="" placeholder="Title">
+      <br><input id='sampleUsername' type="text" name="" value="" placeholder="Username">
+      <br><input id='sampleRating' type="number" min="0" max="10.0" name="GPA" placeholder="Rating"/>
+      <br><input id='sampleComment' type="text" name="" value="" placeholder="Enter Comment (optional)">
+      <button id="sampleDisplay">Submit</button>
+    </form>
+
+
+<!-- END TRY 2 -->
+
+
     <div class="border">
+      <h3>Movie</h3>
       <p>Title: Bee Movie</p>
       <p>Reviewed by: Anonymous</p>
       <p>Rating: 10/10</p>
       <p>Comment: I watch this movie every day before work!</p>
     </div>
     <div class="border">
-      <p>Genre: Nature</p>
+      <h3>Picture</h3>
+      <p>Title: Ice Flower at Dusk</p>
       <p>Reviewed by: Brooklyn Nash</p>
       <p>Rating: 8/10</p>
       <p>Comment: Could use more sunlight :()</p>
     </div>
     <div class="border">
+      <h3>Museum</h3>
       <p>Title: </p>
       <p>Reviewed by: Anonymous</p>
       <p>Rating: 10/10</p>
       <p>Comment: My goodness, I lost my kids</p>
     </div>
     <div id='newReview'>
-      <?php include  ?>
+
+
+<!-- new review is added here -->
+
+
     </div>
-    <h1>Write a Review <button id="myBtn">Write Review</button></h1>
+<!-- BUTTON FOR THE POPUP WINDOW <h1>Write a Review <button id="myBtn">Write Review</button></h1> -->
 
 
-    <!-- The Modal -->
+<!-- START TRY 1 -->
+
+
     <div id="myModal" class="modal">
-
-      <!-- Modal content -->
-      <form onsubmit="display()" id='modelBox' class="modal-content">
+      <form onsubmit="addReview()" id='modelBox' class="modal-content">
         <span class="close">&times;</span>
         <select id='category'>
           <option>Movie</option>
@@ -53,8 +83,12 @@
         <br><input id='username' type="text" name="" value="" placeholder="Username">
         <br><input id='rating' type="number" min="0" max="10.0" name="GPA" placeholder="Rating"/>
         <br><input id='comment' type="text" name="" value="" placeholder="Enter Comment (optional)">
-        <input type="submit" value="Submit Review">
+        <input type="submit" value="submitReview">
       </form>
+
+
+<!-- END TRY 1 -->
+
 
     </div>
     <script>
@@ -85,6 +119,20 @@
     }
     </script>
   </main>
-  <?php include 'footer.inc.php' ?>
+  <?php include 'js/footer.inc.php' ?>
 </body>
 </html>
+
+<!-- $(document).ready(function() {
+  $(".modelBox").submit(function(e) {
+    e.preventDefault();
+
+    var cat = $("#category").val();
+    var title = $("#title").val();
+    var user = $("#username").val();
+    var rating = $("#rating").val();
+    var comment = $("#comment").val();
+    document.getElementById("newReview").append("<div class='border'><p>" + cat + "</p></div>");
+
+  });
+}); -->
--- Project01/js/ReviewPage.js
+++ Project01/js/ReviewPage.js
@@ -1,20 +1,78 @@
 // document.getElementById('modelBox').addEventListener("submit", function() {
 
-function display() {
 
-  var cat = $("#category").val();
-  var title = $("#title").val();
-  var user = $("#username").val();
-  var rating = $("#rating").val();
-  var comment = $("#comment").val();
+// TRY 1
 
-  $("#newReview").append("<div class='border'><p>" + cat + "</p><p>" + title + "</div>");
+
+// function addReview() {
+//
+//   var cat = $("#category").val();
+//   var title = $("#title").val();
+//   var user = $("#username").val();
+//   var rating = $("#rating").val();
+//   var comment = $("#comment").val();
+//
+//   $("#newReview").append("<div class='border'><p>" + cat + "</p><p>" + title + "</div>");
+// }
+
+
+// TRY 2
+
+
+function addSampleReview() {
+  var sampleCat = $("#sampleCategory").val();
+  var sampleTitle = $("#sampleTitle").val();
+  var sampleUser = $("#sampleUsername").val();
+  var sampleRating = $("#sampleRating").val();
+  var sampleComment = $("#sampleComment").val();
+
+  $("#newReview").append("<div class='border'><h3>" + sampleCat + "</h3></div>");
+
 }
+//
+// $(document).ready(function(){
+//   $("#newReview").on('click', addSampleReview);
+// })
+
+
+
+//
+// function addSampleReview() {
+//   var sampleCat = $("#sampleCategory").value();
+//   var sampleTitle = $("#sampleTitle").val();
+//   var sampleUser = $("#sampleUsername").val();
+//   var sampleRating = $("#sampleRating").val();
+//   var sampleComment = $("#sampleComment").val();
+//
+//   $("#newReview").append("<div class='border'><p>" + sampleCat + "</p></div>");
+//
+// }
+//
+// let myButton = document.getElementById('sampleDisplay');
+//
+// myButton.addEventListener('click', addSampleReview);
+
+// $(document).ready(function(){
+//   $("#newReview").on('click', addSampleReview);
+// })
 
 
 
 
 
+// $(document).ready(function() {
+//   $(".modelBox").submit(function(e) {
+//     e.preventDefault();
+//
+//     var cat = $("#category").val();
+//     var title = $("#title").val();
+//     var user = $("#username").val();
+//     var rating = $("#rating").val();
+//     var comment = $("#comment").val();
+//     document.getElementById("newReview").append("<div class='border'><p>" + cat + "</p></div>");
+//
+//   });
+// });
 
 
 

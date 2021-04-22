31d8c55a99cf6ad48b06539182b22b8a348996c1
andersonriley <59894324+andersonriley@users.noreply.github.com>
2021-04-22 12:02:14 -0500

    Update footer.inc.php
    Removed the index href link

--- Project01/js/footer.inc.php
+++ Project01/js/footer.inc.php
@@ -1,5 +1,4 @@
 <footer>
-  <a href="../../index.html">Back to Index</a>
   <p>Justin Dittburner, Nick Wenkus, and Riley Anderson Copyright &copy; 2021 - IT353 - Web Development Technologies </p>
   <script>
   document.write("Last Modified: "+ document.lastModified);

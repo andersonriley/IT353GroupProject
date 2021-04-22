3dee9e0449b75612dc6d3a0818377ede65ad76fc
andersonriley <59894324+andersonriley@users.noreply.github.com>
2021-04-22 11:42:01 -0500

    Update footer.inc.php
    added names to the footer

--- Project01/js/footer.inc.php
+++ Project01/js/footer.inc.php
@@ -1,6 +1,6 @@
 <footer>
   <a href="../../index.html">Back to Index</a>
-  <p>Riley Anderson, Copyright &copy; 2021 - IT353 - Web Development Technologies </p>
+  <p>Justin Dittburner, Nick Wenkus, and Riley Anderson Copyright &copy; 2021 - IT353 - Web Development Technologies </p>
   <script>
   document.write("Last Modified: "+ document.lastModified);
   </script>

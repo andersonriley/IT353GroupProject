3c0941e9937e42dc55fc3a8b0d42d52284f40406
andersonriley <59894324+andersonriley@users.noreply.github.com>
2021-04-22 12:04:28 -0500

    Update footer.inc.php
    added horizontal rule to the footer file

--- Project01/js/footer.inc.php
+++ Project01/js/footer.inc.php
@@ -1,4 +1,5 @@
 <footer>
+  <hr>
   <p>Justin Dittburner, Nick Wenkus, and Riley Anderson Copyright &copy; 2021 - IT353 - Web Development Technologies </p>
   <script>
   document.write("Last Modified: "+ document.lastModified);
